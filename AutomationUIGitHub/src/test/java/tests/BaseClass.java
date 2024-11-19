74cf76c16321cf3296d6a3f6902ba16c7493a8fc
MookappaTN <mookappat@gmail.com>
2024-11-19 22:00:06 +0530

    commit with headless mode code

--- AutomationUIGitHub/src/test/java/tests/BaseClass.java
+++ AutomationUIGitHub/src/test/java/tests/BaseClass.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,7 +19,10 @@ public class BaseClass {
 	@BeforeMethod
 	public void setUp() {
 		
-		driver = new ChromeDriver();
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--headless=new");
+		
+		driver = new ChromeDriver(opt);
 		driver.get("https://practicetestautomation.com/practice-test-login/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
